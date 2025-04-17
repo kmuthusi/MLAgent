69233dfb88f263fa7e3d29a04ed7ea0a76ad2e33
Jacques Kimeu Muthusi <40621081+kmuthusi@users.noreply.github.com>
2025-04-17 10:27:10 +0300

    Update and rename example-MLAgent-iris.py to example-iris-prediction.py

---
+++ example-iris-prediction.py
@@ -0,0 +1,24 @@
+# Example: Using MLAgent Suite to fit models to Iris dataset
+# uncomment to run this to install required packages
+# check-and-install-packages.py
+
+from mlagents import MLAgentClassifier
+
+# load Iris dataset
+from sklearn.datasets import load_iris
+iris = load_iris()
+
+# specify features
+X = pd.DataFrame(iris.data, columns=iris.feature_names)
+feature_names = iris.feature_names
+
+# specify target
+y = iris.target
+
+# load MLAgentClassifier
+agent = MLAgentClassifier()
+
+# apply MLAgent to Iris data
+agent.load_data(X, y, feature_names=feature_names)
+agent.train_and_evaluate(save_path = "models\best_models")
+predictions = agent.predict()
